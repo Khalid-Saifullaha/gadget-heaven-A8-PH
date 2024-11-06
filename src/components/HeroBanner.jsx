3c330357f2995cfb72e31a4cab58f4d02032459b
Your Name <khalidsaifullaha3@gmail.com>
2024-11-06 11:57:41 +0600

    hero banner text change

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -4,13 +4,15 @@ const HeroBanner = () => {
       <div className="hero bg-[#9538E2] rounded-3xl py-20 ">
         <div className="hero-content text-center text-white">
           <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Hello there</h1>
+            <h1 className="text-4xl font-bold">
+              Upgrade Your Tech Accessorize with Gadget Heaven Accessories
+            </h1>
             <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+              Explore the latest gadgets that will take your experience to the
+              next level. From smart devices to the coolest accessories, we have
+              it all!
             </p>
-            <button className="btn rounded-full mb-10">Get Started</button>
+            <button className="btn rounded-full mb-10">Shop Now</button>
           </div>
         </div>
       </div>
